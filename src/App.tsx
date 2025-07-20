83b753e7f52f74e181fbce8e94b44b27d2021737
O.V.Chanuka Prabodha <ckprabodha@gmail.com>
2025-07-20 23:14:57 +0530

    add routing and authentication check in App component; integrate Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import './App.css';
+import {Route, Routes, useNavigate} from "react-router-dom";
+import {DefaultLayout} from "./view/common/DefaultLayout/DefaultLayout.tsx";
+import {Login} from "./view/Pages/Login/Login.tsx";
+import {useEffect} from "react";
+import {isTokenExpired} from "./auth/auth.ts";
+import * as React from "react";
 
 function App() {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token || isTokenExpired(token)) {
+            localStorage.removeItem("token");
+            localStorage.removeItem("refreshToken")
+            navigate("/login")
+        }
+    }, [navigate]);
+
     return (
-        <div className="App">
-            <h1>Welcome to the React App</h1>
-            <p>This is a simple application to demonstrate React functionality.</p>
-        </div>
-    )
+        <Routes>
+            <Route path="/*" element={<DefaultLayout/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
